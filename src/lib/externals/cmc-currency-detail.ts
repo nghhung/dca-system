ec95a43555be7fa0d3ab1392e38da50dfe18683a
nghhung <45676741+nghhung@users.noreply.github.com>
2023-12-11 10:28:17 +0700

    wootrade

--- src/lib/externals/cmc-currency-detail.ts
+++ src/lib/externals/cmc-currency-detail.ts
@@ -1,7 +1,7 @@
 export const getCMCCurrencyDetail = async (slug: string) => {
-  // if (slug === 'WOO') {
-  //   slug = 'wootrade'
-  // }
+  if (slug === 'WOO') {
+    slug = 'woo-network'
+  }
   const res = await fetch(
     `https://api.coinmarketcap.com/data-api/v3/cryptocurrency/detail?slug=${slug}`
   );
